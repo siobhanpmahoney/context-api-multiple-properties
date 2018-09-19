85ebcc1d25a3f3b1b9d5792fbc533be96e94e94c
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-09-19 09:31:01 -0700

    finished for now

--- src/context/DisplayContext.js
+++ src/context/DisplayContext.js
@@ -1,33 +1,35 @@
 import React from 'react'
 
+
+// available font options
 const fontOptions = {
-  sans: {
-    fontFamily: "Helvetica"
-  },
-  serif: {
-    fontFamily: "Times New Roman"
-  },
-  mono: {
-    fontFamily: "Andale Mono"
-  }
+  sans: {fontFamily: "Helvetica"},
+  serif: {fontFamily: "Times New Roman"},
+  mono: {fontFamily: "Andale Mono"}
 }
 
+
+// available color options
 const colorOptions = {
   coral: {color: "#FF5370"},
   blue: {color: "#3D4DF2"},
   aquamarine: {color: "#54d1ff"}
 }
 
+// all options
 export const allSettings = {
   fontOptions,
   colorOptions
 }
 
+// defining default settings
 export const DEFAULT_SETTINGS = {
   fontFamilySetting: fontOptions["sans"],
   textColorSetting: colorOptions["blue"]
 }
 
+
+// defining Context
 export const DisplayContext = React.createContext({
   settings: DEFAULT_SETTINGS,
   toggleSetting: () => {}
